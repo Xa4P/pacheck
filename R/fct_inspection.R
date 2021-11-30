02dc67b88c20f50fea30b78548f7cda680f886bc
Pouwels <x.g.l.v.pouwels@utwente.nl>
2021-11-30 14:56:33 +0100

    Added ice plot function and doc

--- R/fct_inspection.R
+++ R/fct_inspection.R
@@ -8,7 +8,8 @@
 #' @return A dataframe with summary data for selected inputs and outputs.
 #'
 #' @examples
-#' # Generating summary data of all inputs
+#' # Generating summary data of all inputs using the example dataframe
+#' data(df_pa)
 #' df_summary <- generate_sum_stats(df_pa)
 #'
 #' @export
@@ -42,7 +43,7 @@ generate_sum_stats <- function(df,
 
 #' Visualise the distribution of a single parameter
 #'
-#' @description This function helps visualise the distribution of a single parameter
+#' @description This function plots the distribution of a single parameter.
 #'
 #' @param df a dataframe.
 #' @param param character. Name of variable of the dataframe for which the distribution should be plotted.
@@ -52,14 +53,13 @@ generate_sum_stats <- function(df,
 #' @return A ggplot graph.
 #'
 #' @examples
-#' # Generating plot using the default data for the costs of progression-free health state, and bins of 50 euros.
+#' # Generating plot using the example dataframe for the costs of progression-free health state, and bins of 50 euros.
 #' data(df_pa)
 #' vis_1_param(df = df_pa, param = "c_pfs", binwidth = 50)
 #'
 #' @export
 #'
 #'
-# Visualisation of 1 parameter histogram
 vis_1_param <- function(df,
                         param,
                         binwidth = NULL,
@@ -74,6 +74,106 @@ vis_1_param <- function(df,
   } else if(type == "density") {
     p_out <- p + geom_density()
   }
+
+  p_out
+}
+
+
+#' Visualise the distribution of a two parameters
+#'
+#' @description This function plots the distribution of two parameters in a scatterplot.
+#'
+#' @param df a dataframe.
+#' @param param_1 character. Name of variable of the dataframe to be plotted on the x-axis.
+#' @param param_2 character. Name of variable of the dataframe to be plotted on the y-axis.
+#' @param slope numeric. Default is NULL. If different than 0, plots a linear line with intercept 0 and the defined slope.
+#' @param check character. Default is NULL. When set to "param_2 > param_1", plots dot fulfilling the condition in red.
+#'
+#' @return A ggplot graph.
+#'
+#' @examples
+#' # Generating plot using the example dataframe for the costs of progression-free health state, and bins of 50 euros.
+#' data(df_pa)
+#' vis_1_param(df = df_pa, param = "c_pfs", binwidth = 50)
+#'
+#' @export
+#'
+#'
+vis_2_params <- function(df,
+                         param_1,
+                         param_2,
+                         slope = NULL,
+                         check = NULL) {
+  require(ggplot2)
+
+  p <- ggplot(data = df, aes_string(x = param_1, y = param_2)) +
+    theme_bw()
+
+  if(!is.null(slope)) {
+    p <- p + geom_abline(intercept = 0, slope = slope, lty = 2, colour = "orange")
+  }
+
+  if(!is.null(check)) {
+
+    if(check == "param_2 > param_1"){
+
+      df$col <- ifelse(df[, param_2] > df[, param_1], "TRUE", "FALSE")
+      df$col <- as.factor(df$col)
+
+      p_true <- round(length(which(df$col == "TRUE"))/ length(df$col) * 100, 0)
+
+      p_out <- p +
+        geom_point(data = df, aes_string(x = param_1, y = param_2, colour = "col"), shape = 1) +
+        scale_colour_manual(name = "Check",
+                            values = c("TRUE" = "Red",
+                                       "FALSE" = "grey"
+                            )) +
+        annotate("text", label = print(paste("P(TRUE):", p_true, "%")), x = min(df[, param_1]) + (max(df[, param_1]) - min(df[, param_1])) * 0.2,  y = max(df[, param_2]))
+
+    }
+
+  } else {
+    p_out <- p + geom_point(shape = 1, colour = "grey")
+  }
+
+  p_out
+}
+
+#' Incremental cost-effectiveness plot
+#'
+#' @description This function plots the incremental cost-effectiveness plot.
+#'
+#' @param df a dataframe.
+#' @param param_1 character. Name of variable of the dataframe to be plotted on the x-axis.
+#' @param param_2 character. Name of variable of the dataframe to be plotted on the y-axis.
+#' @param wtp numeric. Default is NULL. If different than NULL, plots a linear line with intercept 0 and the defined slope.
+#'
+#' @return A ggplot graph.
+#'
+#' @examples
+#' # Generating plot using the example dataframe, and a willlingness-to-pay threshold of 80,0000 euros.
+#' data(df_pa)
+#' plot_ice(df = df_pa, param_1 = "Inc_QALY", param_1 = "Inc_Costs")
+#'
+#' @export
+#'
+#'
+plot_ice <- function(df,
+                     param_1,
+                     param_2,
+                     wtp = NULL) {
+  require(ggplot2)
+
+  p_out <- ggplot(data = df, aes_string(x = param_1, y = param_2)) +
+    geom_point(shape = 1, colour = "grey") +
+    geom_hline(yintercept = 0) +
+    geom_vline(xintercept = 0) +
+    xlab ("Incremental Effects") +
+    ylab("Incremental Costs") +
+    theme_bw()
+
+
+  if(!is.null(wtp)) {p_out <- p_out + geom_abline(intercept = 0, slope = wtp, lty = 2, colour = "blue")}
 
   p_out
 }
