d2847e0e10aee6f97fbad78b589e9edfb7f33662
Pouwels <x.g.l.v.pouwels@utwente.nl>
2021-12-01 15:44:30 +0100

    Added function fit distributions and separated CEA functions from inspections functions. Update documentations.

---
+++ R/fct_costeffectiveness.R
@@ -0,0 +1,178 @@
+#' Plotting the incremental cost-effectiveness plane.
+#'
+#' @description This function plots the incremental cost-effectiveness plot.
+#'
+#' @param df a dataframe.
+#' @param param_1 character. Name of variable of the dataframe to be plotted on the x-axis.
+#' @param param_2 character. Name of variable of the dataframe to be plotted on the y-axis.
+#' @param wtp numeric. Default is NULL. If different than NULL, plots a linear line with intercept 0 and the defined slope.
+#'
+#' @return A ggplot graph.
+#'
+#' @examples
+#' Generating plot using the example dataframe, and a willlingness-to-pay threshold of 80,0000 euros.
+#' data(df_pa)
+#' plot_ice(df = df_pa, param_1 = "Inc_QALY", param_1 = "Inc_Costs")
+#'
+#' @export
+#'
+#'
+plot_ice <- function(df,
+                     param_1,
+                     param_2,
+                     wtp = NULL) {
+  require(ggplot2)
+  require(scales)
+
+  p_out <- ggplot(data = df, aes_string(x = param_1, y = param_2)) +
+    geom_point(shape = 1, colour = "grey") +
+    geom_hline(yintercept = 0) +
+    geom_vline(xintercept = 0) +
+    xlab ("Incremental Effects") +
+    ylab("Incremental Costs") +
+    scale_y_continuous(labels = dollar_format(prefix = "\u20ac ", suffix = "")) +
+    theme_bw()
+
+
+  if(!is.null(wtp)) {p_out <- p_out + geom_abline(intercept = 0, slope = wtp, lty = 2, colour = "blue")}
+
+  p_out
+}
+
+#' Calculate cost-effectiveness probabilities.
+#'
+#' @description This function calculates the probabilities that each strategy is the most cost effective.
+#'
+#' @param df a dataframe.
+#' @param e_int character. Name of variable of the dataframe containing total effects of the intervention strategy.
+#' @param e_comp character. Name of variable of the dataframe containing total effects of the comparator strategy.
+#' @param c_int character. Name of variable of the dataframe containing total costs of the intervention strategy.
+#' @param c_comp character. Name of variable of the dataframe containing total costs of the comparator strategy.
+#' @param v_wtp vector of numerical values. Vectors of willingness-to-pay threshold for which the probabilities of cost effectiveness have to be defined. Default is 0:100,000 by increments of 1,000.
+#'
+#' @return A ggplot graph.
+#'
+#' @examples
+#' # Calculate probabilities of cost effectiveness using the example dataframe, for willlingness-to-pay thresholds of 0 to 50,0000 euros.
+#' data("df_pa")
+#'calculate_ceac(df_pa,
+#'               e_int = "QALY_int",
+#'               e_comp = "QALY_comp",
+#'               c_int = "Costs_int",
+#'               c_comp = "Costs_comp",
+#'               v_wtp = seq(from = 0, to = 50000, by = 1000))
+#'
+#' @export
+#'
+#'
+calculate_ceac <- function (df, e_int, e_comp, c_int, c_comp, v_wtp = seq(from = 0, to = 100000, by = 1000)){
+
+  m_res <- matrix(NA, nrow = length(v_wtp), ncol = 3,
+                  dimnames = list(v_wtp, c("WTP_threshold", "Prob_int", "Prob_comp")))
+  m_tmp <- matrix(NA, nrow = length(df[, e_int]), ncol = 2, #temp stores NB for each iteration
+                  dimnames = list(c(1:length(df[, e_int])),c("res_int", "res_comp")))
+
+  for (i in 1:length(v_wtp)){
+    m_tmp[,"res_int"]   <- df[, e_int] * v_wtp[i] - df[, c_int]   #compute NB for intervention at wtp threshold
+    m_tmp[,"res_comp"]  <- df[, e_comp] * v_wtp[i] - df[, c_comp] #compute NB for comparator at wtp threshold
+
+    num_CE <- length(which(m_tmp[, "res_int"] > m_tmp[, "res_comp"])) #number of iterations in which intervention is cost effective
+    p_trt  <- num_CE/length(df[, e_int]) #probability trt has a higher NB than comp
+
+    m_res[i,] <- cbind(v_wtp[i], p_trt, 1 - p_trt) #vector of wtp and probabilities is stored
+  }
+
+  rownames(m_res) <- NULL
+
+  return(m_res)
+}
+
+#' Plot cost-effectiveness acceptability curves.
+#'
+#' @description This function calculates the probabilities that each strategy is the most cost effective.
+#'
+#' @param df a dataframe.
+#' @param wtp vector of numerical values. Name of variable of the dataframe containing the willingness-to-pay thresholds at which the probability of cost effectiveness have been defined.
+#'
+#' @return A ggplot graph.
+#'
+#' @examples
+#' # Plot CEAC based on results from calculate_ceac()
+#' data("df_pa")
+#' m_res_ceac <- calculate_ceac(df_pa,
+#'               e_int = "QALY_int",
+#'               e_comp = "QALY_comp",
+#'               c_int = "Costs_int",
+#'               c_comp = "Costs_comp")
+#' df_ceac_p <- as.data.frame(m_res_ceac)
+#'
+#' plot_ceac(df = df_ceac_p,
+#'           v_wtp = "WTP_threshold")
+#'
+#' @export
+#'
+#'
+plot_ceac <- function(df,
+                      wtp) {
+  require(ggplot2)
+  require(reshape2)
+  require(scales)
+
+  df_graph <- melt(data = df_ceac_p, id.vars = wtp)
+
+  p_out <- ggplot(data = df_graph, aes_string(x = wtp, y = "value", colour = "variable")) +
+    geom_line() +
+    xlab ("Willingness-to-pay threshold") +
+    ylab("Probability of cost effectiveness") +
+    scale_x_continuous(labels = dollar_format(prefix = "\u20ac ", suffix = "")) +
+    scale_colour_manual(name = "Strategy",
+                        values = c(Prob_int = "grey", Prob_comp = "orange")) +
+    theme_bw()
+
+  p_out
+}
+
+#' Calculate NMB and NHB.
+#'
+#' @description This function calculates the Net Monetary Benefits (NMB) and Net Health Benefits (NHB) for each strategy and the incremental NMB and NHB.
+#'
+#' @param df a dataframe.
+#' @param e_int character. Name of variable of the dataframe containing total effects of the intervention strategy.
+#' @param e_comp character. Name of variable of the dataframe containing total effects of the comparator strategy.
+#' @param c_int character. Name of variable of the dataframe containing total costs of the intervention strategy.
+#' @param c_comp character. Name of variable of the dataframe containing total costs of the comparator strategy.
+#' @param wtp numerical values. Willingness-to-pay thresholds to use for NMB and NHB calculations.
+#'
+#' @return A dataframe.
+#'
+#' @examples
+#' # Calculate NB's at a willingness-to-pay threshold of 80000 per unit of effects
+#' data("df_pa")
+#' calculate_nb(df_pa,
+#'              e_int = "QALY_int",
+#'              e_comp = "QALY_comp",
+#'              c_int = "Costs_int",
+#'              c_comp = "Costs_comp",
+#'              wtp = 80000)
+#'
+#' @export
+#'
+#'
+calculate_nb <- function(df,
+                         e_int,
+                         e_comp,
+                         c_int,
+                         c_comp,
+                         wtp) {
+
+  df_out <- data.frame(df,
+                       NMB_int = df[, e_int] * wtp - df[, c_int],
+                       NMB_comp = df[, e_comp] * wtp - df[, c_comp],
+                       iNMB = (df[, e_int] - df[, e_comp]) * wtp - (df[, c_int] - df[, c_comp]),
+                       NHB_int = df[, e_int] - df[, c_int] / wtp,
+                       NHB_comp = df[, e_comp] - df[, c_comp] / wtp,
+                       iNHB = (df[, e_int] - df[, e_comp]) - (df[, c_int] - df[, c_comp]) / wtp
+  )
+
+  return(df_out)
+}
--- R/fct_inspection.R
+++ R/fct_inspection.R
@@ -49,11 +49,14 @@ generate_sum_stats <- function(df,
 #' @param param character. Name of variable of the dataframe for which the distribution should be plotted.
 #' @param binwidth numeric. Determine the width of the bins to use, only applied in combination with "histogram". Default is 30 bins.
 #' @param type character. Determine which plot to return: "histogram" for a histogram, "density" for a density plot. Default is "histogram".
+#' @param dist character or vector of character. Determine which distribution to fit on the density plot.
+#'
+#' @details The available distributions are: "norm" (normal), "beta", "gamma", "lnorm" (lognormal)
 #'
 #' @return A ggplot graph.
 #'
 #' @examples
-#' # Generating plot using the example dataframe for the costs of progression-free health state, and bins of 50 euros.
+#' # Generating histogram using the example dataframe for the costs of progression-free health state, and bins of 50 euros.
 #' data(df_pa)
 #' vis_1_param(df = df_pa, param = "c_pfs", binwidth = 50)
 #'
@@ -63,21 +66,95 @@ generate_sum_stats <- function(df,
 vis_1_param <- function(df,
                         param,
                         binwidth = NULL,
-                        type = "histogram") {
+                        type = "histogram",
+                        dist = c("norm", "beta", "gamma", "lnorm")) {
   require(ggplot2)
+  require(fitdistrplus)
+
+  if("beta" %in% dist) {beta_dist <- fitdist(df[, param], distr = "beta")}
+  if("gamma" %in% dist) {gamma_dist <- fitdist(df[, param], distr = "gamma")}
+  if("norm" %in% dist) {norm_dist <- fitdist(df[, param], distr = "norm")}
+  if("lnorm" %in% dist) {lnorm_dist <- fitdist(df[, param], distr = "lnorm")}
+
+  v_dist <- c("Original", dist)
+
+  df_legend <- data.frame(
+    dist_call = c("Original", "norm", "beta", "gamma", "lnorm"),
+    col = c("Original" = "black", "Normal" = "orange", "Beta" = "red", "Gamma" = "blue", "Lognormal" = "green")
+  )
+
+  df_legend <- df_legend[which(df_legend$dist_call %in% v_dist),]
 
   p <- ggplot(data = df, aes_string(x = param)) +
     theme_bw()
 
   if(type == "histogram") {
     p_out <- p + geom_histogram(binwidth = binwidth)
+
   } else if(type == "density") {
-    p_out <- p + geom_density()
+    p_out <- p +
+      geom_histogram(aes(y = ..density..),
+                     colour = "grey",
+                     fill = "lightgrey") +
+      geom_density(aes(colour = "Data"))
+
+    if("beta" %in% dist) {
+      df_beta <- data.frame(
+        x = seq(from = min(df[, param]), to = max(df[, param]), by = 0.001),
+        y = dbeta(seq(from = min(df[, param]), to = max(df[, param]), by = 0.001), beta_dist$estimate[[1]], beta_dist$estimate[[2]]))
+
+      p_out <- p_out + geom_line(data = df_beta,
+                                 aes(x = x,
+                                     y = y,
+                                     colour = "Beta")
+      )
+    }
+
+    if("gamma" %in% dist) {
+      df_gamma <- data.frame(
+        x = seq(from = min(df[, param]), to = max(df[, param]), by = 0.001),
+        y = dgamma(seq(from = min(df[, param]), to = max(df[, param]), by = 0.001), gamma_dist$estimate[[1]], gamma_dist$estimate[[2]]))
+
+      p_out <- p_out + geom_line(data = df_gamma,
+                                 aes(x = x,
+                                     y = y,
+                                     colour = "Gamma")
+      )
+    }
+
+    if("norm" %in% dist) {
+      df_norm <- data.frame(
+        x = seq(from = min(df[, param]), to = max(df[, param]), by = 0.001),
+        y = dnorm(seq(from = min(df[, param]), to = max(df[, param]), by = 0.001), norm_dist$estimate[[1]], norm_dist$estimate[[2]]))
+
+      p_out <- p_out + geom_line(data = df_norm,
+                                 aes(x = x,
+                                     y = y,
+                                     colour = "Normal"),
+      )
+    }
+
+    if("lnorm" %in% dist) {
+      df_lnorm <- data.frame(
+        x = seq(from = min(df[, param]), to = max(df[, param]), by = 0.001),
+        y = dlnorm(seq(from = min(df[, param]), to = max(df[, param]), by = 0.001), lnorm_dist$estimate[[1]], lnorm_dist$estimate[[2]]))
+
+      p_out <- p_out + geom_line(data = df_lnorm,
+                                 aes(x = x,
+                                     y = y,
+                                     colour = "Lognormal"),
+      )
+    }
+
   }
+
+  p_out <- p_out +
+    scale_colour_manual(name = "Distributions",
+                        values = df_legend$col) +
+    theme(legend.key = element_rect(fill = "lightgrey"))
 
   p_out
 }
-
 
 #' Visualise the distribution of a two parameters
 #'
@@ -139,181 +216,72 @@ vis_2_params <- function(df,
   p_out
 }
 
-#' Plotting the incremental cost-effectiveness plane.
+
+
+#' Fit distribution to parameter
 #'
-#' @description This function plots the incremental cost-effectiveness plot.
+#' @description This function fits statistical distributions to a parameter.
 #'
 #' @param df a dataframe.
-#' @param param_1 character. Name of variable of the dataframe to be plotted on the x-axis.
-#' @param param_2 character. Name of variable of the dataframe to be plotted on the y-axis.
-#' @param wtp numeric. Default is NULL. If different than NULL, plots a linear line with intercept 0 and the defined slope.
+#' @param param character. Name of variable of the dataframe on which to fit the distributions.
+#' @param dist character or vector of character. Determine which distribution to fit on the density plot.
 #'
-#' @return A ggplot graph.
+#' @details The available distributions are: "norm" (normal), "beta", "gamma", "lnorm" (lognormal). The arguments of the lists are "AIC" which contains the Akaike Information Criteria for each fitted distribution and "Dist_parameters" which contains the parameters of the fitted distributions.
+#'
+#' @return A list with two arguments
 #'
 #' @examples
-#' Generating plot using the example dataframe, and a willlingness-to-pay threshold of 80,0000 euros.
+#' # Fitting normal and beta distribution to the "u_pfs" variable of the example dataframe.
 #' data(df_pa)
-#' plot_ice(df = df_pa, param_1 = "Inc_QALY", param_1 = "Inc_Costs")
+#' fit_dist(df = df_pa,
+#'          param = "u_pfs",
+#'          dist = c("norm", "beta"))
 #'
 #' @export
 #'
 #'
-plot_ice <- function(df,
-                     param_1,
-                     param_2,
-                     wtp = NULL) {
-  require(ggplot2)
-  require(scales)
+fit_dist <- function(df,
+                     param,
+                     dist = c("norm", "beta", "gamma", "lnorm")) {
 
-  p_out <- ggplot(data = df, aes_string(x = param_1, y = param_2)) +
-    geom_point(shape = 1, colour = "grey") +
-    geom_hline(yintercept = 0) +
-    geom_vline(xintercept = 0) +
-    xlab ("Incremental Effects") +
-    ylab("Incremental Costs") +
-    scale_y_continuous(labels = dollar_format(prefix = "\u20ac ", suffix = "")) +
-    theme_bw()
+  require(fitdistrplus)
+
+  l_dist <- vector(mode = "list", length = length(dist))
+  names(l_dist) <- dist
+
+  l_out <- vector(mode = "list", length = 2)
+  names(l_out) <- c("AIC", "Dist_parameters")
+
+  m_stats <- matrix(NA,
+                    ncol = 2,
+                    nrow = length(dist),
+                    dimnames = list(NULL,
+                                    c("Distribution", "AIC")))
+  m_stats[, "Distribution"] <- dist
+
+  m_params <- matrix(NA,
+                     ncol = 5,
+                     nrow = length(dist),
+                     dimnames = list(NULL,
+                                     c("Distribution", "Name_param_1", "Value_param_1", "Name_param_2", "Value_param_2")))
+  m_params[, "Distribution"] <- dist
 
 
-  if(!is.null(wtp)) {p_out <- p_out + geom_abline(intercept = 0, slope = wtp, lty = 2, colour = "blue")}
-
-  p_out
-}
-
-#' Calculate cost-effectiveness probabilities.
-#'
-#' @description This function calculates the probabilities that each strategy is the most cost effective.
-#'
-#' @param df a dataframe.
-#' @param e_int character. Name of variable of the dataframe containing total effects of the intervention strategy.
-#' @param e_comp character. Name of variable of the dataframe containing total effects of the comparator strategy.
-#' @param c_int character. Name of variable of the dataframe containing total costs of the intervention strategy.
-#' @param c_comp character. Name of variable of the dataframe containing total costs of the comparator strategy.
-#' @param v_wtp vector of numerical values. Vectors of willingness-to-pay threshold for which the probabilities of cost effectiveness have to be defined. Default is 0:100,000 by increments of 1,000.
-#'
-#' @return A ggplot graph.
-#'
-#' @examples
-#' # Calculate probabilities of cost effectiveness using the example dataframe, for willlingness-to-pay thresholds of 0 to 50,0000 euros.
-#' data("df_pa")
-#'calculate_ceac(df_pa,
-#'               e_int = "QALY_int",
-#'               e_comp = "QALY_comp",
-#'               c_int = "Costs_int",
-#'               c_comp = "Costs_comp",
-#'               v_wtp = seq(from = 0, to = 50000, by = 1000))
-#'
-#' @export
-#'
-#'
-calculate_ceac <- function (df, e_int, e_comp, c_int, c_comp, v_wtp = seq(from = 0, to = 100000, by = 1000)){
-
-  m_res <- matrix(NA, nrow = length(v_wtp), ncol = 3,
-                dimnames = list(v_wtp, c("WTP_threshold", "Prob_int", "Prob_comp")))
-  m_tmp <- matrix(NA, nrow = length(df[, e_int]), ncol = 2, #temp stores NB for each iteration
-                dimnames = list(c(1:length(df[, e_int])),c("res_int", "res_comp")))
-
-  for (i in 1:length(v_wtp)){
-    m_tmp[,"res_int"]   <- df[, e_int] * v_wtp[i] - df[, c_int]   #compute NB for intervention at wtp threshold
-    m_tmp[,"res_comp"]  <- df[, e_comp] * v_wtp[i] - df[, c_comp] #compute NB for comparator at wtp threshold
-
-    num_CE <- length(which(m_tmp[, "res_int"] > m_tmp[, "res_comp"])) #number of iterations in which intervention is cost effective
-    p_trt  <- num_CE/length(df[, e_int]) #probability trt has a higher NB than comp
-
-    m_res[i,] <- cbind(v_wtp[i], p_trt, 1 - p_trt) #vector of wtp and probabilities is stored
+  for (i in 1:length(dist)){
+    l_dist[[i]] <- fitdist(df[, param], distr = dist[i])
   }
 
-  rownames(m_res) <- NULL
+  for (i in 1:length(dist)){
+    m_stats[i, "AIC"] <- round(l_dist[[i]]$aic)
+  }
 
-  return(m_res)
-}
+  for (i in 1:length(dist)){
+    m_params[i, 2:ncol(m_params)] <- c(names(l_dist[[i]]$estimate)[1], round(l_dist[[i]]$estimate[1], 2), names(l_dist[[i]]$estimate)[2], round(l_dist[[i]]$estimate[2], 2))
+  }
 
-#' Plot cost-effectiveness acceptability curves.
-#'
-#' @description This function calculates the probabilities that each strategy is the most cost effective.
-#'
-#' @param df a dataframe.
-#' @param wtp vector of numerical values. Name of variable of the dataframe containing the willingness-to-pay thresholds at which the probability of cost effectiveness have been defined.
-#'
-#' @return A ggplot graph.
-#'
-#' @examples
-#' # Plot CEAC based on results from calculate_ceac()
-#' data("df_pa")
-#' m_res_ceac <- calculate_ceac(df_pa,
-#'               e_int = "QALY_int",
-#'               e_comp = "QALY_comp",
-#'               c_int = "Costs_int",
-#'               c_comp = "Costs_comp")
-#' df_ceac_p <- as.data.frame(m_res_ceac)
-#'
-#' plot_ceac(df = df_ceac_p,
-#'           v_wtp = "WTP_threshold")
-#'
-#' @export
-#'
-#'
-plot_ceac <- function(df,
-                      wtp) {
-  require(ggplot2)
-  require(reshape2)
-  require(scales)
+  l_out[[1]] <- as.data.frame(m_stats)
+  l_out[[2]] <- as.data.frame(m_params)
 
-  df_graph <- melt(data = df_ceac_p, id.vars = wtp)
+  return(l_out)
 
-  p_out <- ggplot(data = df_graph, aes_string(x = wtp, y = "value", colour = "variable")) +
-    geom_line() +
-    xlab ("Willingness-to-pay threshold") +
-    ylab("Probability of cost effectiveness") +
-    scale_x_continuous(labels = dollar_format(prefix = "\u20ac ", suffix = "")) +
-    scale_colour_manual(name = "Strategy",
-                        values = c(Prob_int = "grey", Prob_comp = "orange")) +
-    theme_bw()
-
-  p_out
-}
-
-#' Calculate NMB and NHB.
-#'
-#' @description This function calculates the Net Monetary Benefits (NMB) and Net Health Benefits (NHB) for each strategy and the incremental NMB and NHB.
-#'
-#' @param df a dataframe.
-#' @param e_int character. Name of variable of the dataframe containing total effects of the intervention strategy.
-#' @param e_comp character. Name of variable of the dataframe containing total effects of the comparator strategy.
-#' @param c_int character. Name of variable of the dataframe containing total costs of the intervention strategy.
-#' @param c_comp character. Name of variable of the dataframe containing total costs of the comparator strategy.
-#' @param wtp numerical values. Willingness-to-pay thresholds to use for NMB and NHB calculations.
-#'
-#' @return A dataframe.
-#'
-#' @examples
-#' # Calculate NB's at a willingness-to-pay threshold of 80000 per unit of effects
-#' data("df_pa")
-#' calculate_nb(df_pa,
-#'              e_int = "QALY_int",
-#'              e_comp = "QALY_comp",
-#'              c_int = "Costs_int",
-#'              c_comp = "Costs_comp",
-#'              wtp = 80000)
-#'
-#' @export
-#'
-#'
-calculate_nb <- function(df,
-                    e_int,
-                    e_comp,
-                    c_int,
-                    c_comp,
-                    wtp) {
-
-  df_out <- data.frame(df,
-                       NMB_int = df[, e_int] * wtp - df[, c_int],
-                       NMB_comp = df[, e_comp] * wtp - df[, c_comp],
-                       iNMB = (df[, e_int] - df[, e_comp]) * wtp - (df[, c_int] - df[, c_comp]),
-                       NHB_int = df[, e_int] - df[, c_int] / wtp,
-                       NHB_comp = df[, e_comp] - df[, c_comp] / wtp,
-                       iNHB = (df[, e_int] - df[, e_comp]) - (df[, c_int] - df[, c_comp]) / wtp
-                       )
-
-  return(df_out)
 }
