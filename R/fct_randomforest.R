bbe9145231c8440e6349ce2404cf8cc496b1a916
Mart Staarink <mart.staarink@hotmail.com>
2023-12-13 15:16:24 +0100

    feature: function variables added

--- R/fct_randomforest.R
+++ R/fct_randomforest.R
@@ -1,12 +1,27 @@
-#' Title
+#' Fit random forest metamodel
 #'
-#' @param x
-#' @param split
+#' @param df
+#' @param y_var
+#' @param x_vars
+#' @param standardise
+#' @param x_poly_2
+#' @param x_poly_3
+#' @param x_exp
+#' @param x_log
+#' @param x_inter
 #'
 #' @return
 #' @export
 #'
 #' @examples
-fit_rf_metamodel <- function(x, split) {
-  strsplit(x, split = split)[[1]]
+fit_rf_metamodel <- function(df,
+                             y_var = NULL,
+                             x_vars = NULL,
+                             standardise = FALSE,
+                             x_poly_2 = NULL,
+                             x_poly_3 = NULL,
+                             x_exp = NULL,
+                             x_log = NULL,
+                             x_inter = NULL) {
+
 }
