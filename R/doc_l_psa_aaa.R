04bb49dc2a807bc5b03b89b4b642eed20a147679
Xa4P <x.g.l.v.pouwels@utwente.nl>
2025-12-18 09:41:17 +0100

    fix: update source of datasets

--- R/doc_l_psa_aaa.R
+++ R/doc_l_psa_aaa.R
@@ -2,5 +2,5 @@
 #' @format The dataframe contains the 1,000 probabilistic inputs and outputs obtained by executin the Main file_men 30years_FullModel.R' script (for 1,000 individuals) under the 'models' folder on the \code{\link[https://github.com/mikesweeting/AAA_DES_model]{github repository}} of the model.
 #' @description A list containing 1,000 sets of probabilistic inputs and outputs from a discrete event simulation developed in R.
 #' @source Sweeting MJ, Masconi KL, Jones E, Ulug P, Glover MJ, Michaels JA, Bown MJ, Powell JT, Thompson SG. Analysis of clinical benefit, harms, and cost-effectiveness of screening women for abdominal aortic aneurysm. Lancet. 2018 Aug 11;392(10146):487-495. doi: 10.1016/S0140-6736(18)31222-4.
-#' @source Github repository of the model: \code{\link[https://github.com/mikesweeting/AAA_DES_model]{https://github.com/mikesweeting/AAA_DES_model}}
+#' @source https://github.com/mikesweeting/AAA_DES_model
 "l_psa_aaa"
