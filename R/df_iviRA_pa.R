a5ab6b3ded205825390db26cdcae503a56eed5e0
Xa4P <x.g.l.v.pouwels@utwente.nl>
2025-12-19 12:04:28 +0100

    fix: update reference to iviRA package to prevent note fix: update aaa case study vignette

--- R/df_iviRA_pa.R
+++ R/df_iviRA_pa.R
@@ -1,6 +1,7 @@
 #' Dataframe of inputs and outputs of a health economic model developed and evaluated with the iviRA R package for testing
-#' @description A dataframe containing 1,000 sets of probabilistic inputs and outputs of a individual-level health state transition model developed using \code{\link[iviRA]{iviRA}} package.
-#' @format See the documentation of the \code{\link[iviRA:sample_pars]{iviRA::sample_pars}} function for details.
+#' @description A dataframe containing 1,000 sets of probabilistic inputs and outputs of a individual-level health state transition model developed using iviRA package available on Github.
+#' @format See the documentation of the `sample_pars` function of the iviRA package for details.
 #' @source Incerti D, Curtis JR, Shafrin J, Lakdawalla DN, Jansen JP (2019). “ A Flexible Open-Source Decision Model for Value Assessment of Biologic Treatment for Rheumatoid Arthritis.” PharmacoEconomics. doi: 10.1007/s40273-018-00765-2.
 #' @source Link to Github repository: \url{https://innovationvalueinitiative.github.io/IVI-RA/index.html}
+#' @source Link to iviRA website: \url{https://innovationvalueinitiative.github.io/IVI-RA/}
 "df_iviRA_pa"
